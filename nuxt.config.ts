f559391742c763a99b9ca8a9cae5bcb14bf8dd95
Yilee <yileefrontend@gmail.com>
2025-01-13 23:31:35 +0800

    [環境]quasar安裝

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint', 'nuxt-quasar-ui'],
+  quasar: {
+    plugins: [
+      'Dialog',
+      'Loading'
+    ]
+  }
 });
