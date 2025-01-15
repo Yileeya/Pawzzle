8dddcae23c5fa97e191224b7dbd1c1e76a69ee20
Yilee <yileefrontend@gmail.com>
2025-01-15 23:45:02 +0800

    [page]404頁面

---
+++ pages/[...slug].vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// https://ithelp.ithome.com.tw/articles/10338164
+const event = useRequestEvent();
+setResponseStatus(event, 404);
+
+definePageMeta({
+  layout: 'empty'
+});
+</script>
+
+<template>
+  <div>
+    <h1>此頁面不存在！</h1>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
