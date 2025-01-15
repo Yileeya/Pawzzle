3078baa3c95f8f34fa55db73a268cc408bb7086b
Yilee <yileefrontend@gmail.com>
2025-01-15 22:11:12 +0800

    [環境]nuxt-icons安裝

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,24 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', 'nuxt-quasar-ui'],
+  modules: ['@nuxt/eslint', 'nuxt-quasar-ui', 'nuxt-icons'],
   quasar: {
     plugins: [
       'Dialog',
       'Loading'
+    ]
+  },
+  vite: { //https://github.com/gitFoxCode/nuxt-icons/issues/56#issuecomment-2457905778
+    plugins: [
+      {
+        name: 'vite-plugin-glob-transform',
+        transform(code: string, id: string) {
+          if (id.includes('nuxt-icons')) {
+            return code.replace(/as:\s*['"]raw['"]/g, 'query: "?raw", import: "default"');
+          }
+          return code;
+        }
+      }
     ]
   }
 });
