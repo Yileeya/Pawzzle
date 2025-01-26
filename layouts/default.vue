fdebec93c19e0eb93902e2f4e31ec05741d7c3c3
Yilee <yileefrontend@gmail.com>
2025-01-26 11:16:14 +0800

    [layout]layout基本框架

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,31 @@
+
+<script setup lang="ts">
+const leftDrawerOpen = ref<boolean>(false);
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
+
 <template>
-  <div class="layout">
-    <header>header</header>
-    <slot />
-    <footer>footer</footer>
-  </div>
+  <q-layout view="hHh lpR fFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          header here
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" side="left" behavior="mobile">
+      drawer content
+    </q-drawer>
+
+    <q-page-container>
+      <slot />
+    </q-page-container>
+
+    <q-footer> footer here </q-footer>
+  </q-layout>
 </template>
