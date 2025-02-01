e85e68bc41dfe10ae14cb0558ea356da6f9106cd
Yilee <yileefrontend@gmail.com>
2025-02-01 15:21:04 +0800

    [環境]css import調整

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: {enabled: true},
-  css: ['@/assets/css/base.scss'],
+  css: [
+    'quasar/dist/quasar.prod.css',
+    '@/assets/css/base.scss'
+  ],
   modules: [
     '@nuxt/eslint', 'nuxt-quasar-ui', 'nuxt-icons', 'nuxt-aos',
     [
