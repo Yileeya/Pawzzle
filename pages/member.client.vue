5f07965383bdb646c369466bb9866bee62eb71e4
Yilee <yileefrontend@gmail.com>
2025-02-28 12:37:58 +0800

    [0228]member page title

--- pages/member.client.vue
+++ pages/member.client.vue
@@ -4,15 +4,93 @@ definePageMeta({
 });
 </script>
 <template>
-  <div>
-    <h1>會員頁面</h1>
-    <div>
-      <p>歡迎來到會員中心</p>
-    </div>
-    <div>
-      <NuxtLink to="/member/user">客戶資料</NuxtLink>
-      <NuxtLink to="/member/order">訂單頁</NuxtLink>
+  <div class="member-page max-page-width">
+    <div class="menu-block">
+      <div class="title-block">
+        <h1 class="page-title">會員專區</h1>
+        <div class="user-email">
+          <nuxt-icon name="circle-user-solid" filled />
+          rita841027@gmail.com
+        </div>
+      </div>
+      <div class="tab-block">
+        <NuxtLink to="/member/user">
+          <nuxt-icon name="address-card-regular" filled />個人資料
+        </NuxtLink>
+        <NuxtLink to="/member/order">
+          <nuxt-icon name="rectangle-list-regular" filled />我的訂單
+        </NuxtLink>
+      </div>
     </div>
     <NuxtPage />
   </div>
 </template>
+
+<style scoped lang="scss">
+.member-page {
+  .menu-block {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row-reverse;
+    align-items: center;
+    padding: 30px 0;
+  }
+  .title-block {
+    display: flex;
+    align-items: flex-end;
+    gap: 10px 30px;
+    .page-title {
+      font-size: 1.875rem;
+      margin: 0;
+      font-weight: 400;
+      line-height: 1;
+    }
+    .user-email {
+      font-size: 1.125rem;
+      :deep(.nuxt-icon) {
+        font-size: 1.125rem;
+        margin-right: 2px;
+        fill: var(--secondary-color);
+        svg {
+          margin-bottom: 2px;
+        }
+      }
+    }
+  }
+
+  .tab-block {
+    font-size: 1.125rem;
+    display: flex;
+    a {
+      text-decoration: none;
+      color: var(--secondary-color);
+      padding-bottom: 3px;
+      width: 150px;
+      text-align: center;
+      border-radius: 5px;
+      :deep(.nuxt-icon) {
+        font-size: 24px;
+        fill: var(--secondary-color);
+        margin-right: 5px;
+        svg {
+          margin-bottom: 0;
+        }
+      }
+      &.router-link-active {
+        font-weight: 500;
+        color: var(--secondary-color);
+        border-bottom: 3px solid var(--secondary-color);
+        border-radius: 0;
+      }
+      &:hover {
+        background: var(--secondary-color);
+        color: white;
+        border-radius: 5px;
+        .nuxt-icon {
+          fill: white;
+        }
+      }
+    }
+  }
+}
+</style>
