e4552d23d908579f1a4aa60ea75fd309bc4112a7
Yilee <yileefrontend@gmail.com>
2025-03-15 13:58:57 +0800

    [api]api攔截器設定，錯誤處理

--- plugins/api.ts
+++ plugins/api.ts
@@ -1,8 +1,50 @@
+import DialogComponent from '@/components/common/Dialog.vue';
+import { Dialog } from 'quasar';
+
+interface ApiErrorResponse {
+  status: boolean;
+  message: string[];
+}
+
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig();
+  
   const api = $fetch.create({
-    baseURL: config.public.apiUrl as string
-  });
+    baseURL: config.public.apiUrl as string,
+    onRequest({ request, options, error }) {
+      const token = useCookie('token');
+      if (token) {
+        options.headers.set('Authorization', `Bearer ${token.value}`);
+      }
+    },
+    onResponse({ response }) {
+      // 拿取 data 層級的資料，若沒有則直接回傳原始資料
+      if (response._data && response._data.data) {
+        response._data = response._data.data;
+      }
+      return response._data;
+    },
+    async onResponseError(response) {
+      const data = response.response._data;
+
+      let status = false;
+      let message: string[] = ['出現未知錯誤，請聯繫管理員。'];
+
+      status = data.status !== 'fail';
+      if (data.message)
+        message = Array.isArray(data.message) ? data.message : [data.message];
+
+      Dialog.create({
+        component: DialogComponent,
+        componentProps: {
+          mode: 'error',
+          content: message
+        }
+      });
+
+      throw { status, message } as ApiErrorResponse;
+    }
+  }) as typeof $fetch;
 
   return {
     provide: {
---
+++ types/nuxt.d.ts
@@ -0,0 +1,13 @@
+import type { $Fetch } from 'ofetch';
+
+declare module '#app' {
+    interface NuxtApp {
+        $api: $Fetch
+    }
+}
+declare module 'nuxt/app' {
+    interface NuxtApp {
+        $api: typeof $fetch;
+    }
+}
+export {};
