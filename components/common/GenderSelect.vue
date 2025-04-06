05b87a5f7db385a3795f98453a28977a86f1e802
Yilee <yileefrontend@gmail.com>
2025-04-06 16:31:55 +0800

    [components]add gender select

---
+++ components/common/GenderSelect.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const gender = defineModel<string>({ required: true });
+
+const typeListStore = useTypeListStore();
+const { genderList } = storeToRefs(typeListStore);
+
+const optionsData = computed(() => {
+  const genderOptions = Object.entries(genderList.value).map(
+    ([key, value]) => ({
+      id: key,
+      name: value
+    })
+  );
+  return [{ id: '', name: '請選擇', disable: true }, ...genderOptions];
+});
+</script>
+
+<template>
+  <q-select
+    v-model="gender"
+    :options="optionsData"
+    option-value="id"
+    option-label="name"
+    label="性別"
+    transition-show="scale"
+    transition-hide="scale"
+    emit-value
+    map-options
+    class="el-select"
+    popup-content-class="el-select-pupup-content"
+    options-selected-class="options-selected"
+  />
+</template>
+
+<style lang="scss"></style>
