0b61ddbbe20ad1d539a1aad4d4d66e3079309fd8
Yilee <yileefrontend@gmail.com>
2025-04-13 22:25:04 +0800

    [layout]adj loading css style

--- components/AppLoading.vue
+++ components/AppLoading.vue
@@ -27,15 +27,4 @@ const { isLoading } = storeToRefs(useUiUxStore());
   align-items: center;
   justify-content: center;
 }
-
-/* 淡入淡出動畫定義 */
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
 </style>
